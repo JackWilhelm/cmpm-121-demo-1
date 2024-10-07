238447f92408cbb87df51bf55d2236641df4f484
JackWilhelm <jgwilhel@ucsc.edu>
2024-10-07 15:50:33 -0700

    Step 1 complete

--- src/main.ts
+++ src/main.ts
@@ -8,3 +8,8 @@ document.title = gameName;
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+//Button
+const button = document.createElement("button");
+button.innerHTML = "🍬";
+app.append(button);
