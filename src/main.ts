9c1ab3f358979fdb13d88631f514139cef9fcd51
JackWilhelm <jgwilhel@ucsc.edu>
2024-10-07 16:21:39 -0700

    Step 2 complete

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,30 @@ const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
+//Counter
+let counter: number = 0;
+function updateCounter() {
+    counter++;
+    console.log(counter);
+    if (counter == 1) {
+        divElement.textContent = `${counter} treat!`; //proper grammar
+    } else {
+        divElement.textContent = `${counter} treats!`;
+    }
+    
+
+}
+
 //Button
-const button = document.createElement("button");
+const button: HTMLButtonElement = document.createElement("button");
 button.innerHTML = "🍬";
 app.append(button);
+button.addEventListener("click", () => {updateCounter()});
+
+//divElement
+const divElement: HTMLDivElement = document.createElement("div");
+divElement.id = "Trick or Treat!"; //Unit Label
+divElement.textContent = `${counter} treats!`;
+app.append(divElement);
+
+
