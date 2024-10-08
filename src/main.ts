8103fcd311f1e7a73ffd577f71638c94d3548a5b
JackWilhelm <jgwilhel@ucsc.edu>
2024-10-07 17:48:56 -0700

    Step 4 complete

--- src/main.ts
+++ src/main.ts
@@ -11,14 +11,11 @@ app.append(header);
 
 //Counter
 let counter: number = 0;
+let lastTime: number = performance.now();
 function updateCounter() {
   counter++;
   console.log(counter);
-  if (counter == 1) {
-    divElement.textContent = `${counter} treat!`; //proper grammar
-  } else {
-    divElement.textContent = `${counter} treats!`;
-  }
+  updateDivElement();
 }
 
 //Button
@@ -32,8 +29,25 @@ button.addEventListener("click", () => {
 //divElement
 const divElement: HTMLDivElement = document.createElement("div");
 divElement.id = "Trick or Treat!"; //Unit Label
-divElement.textContent = `${counter} treats!`;
+divElement.innerHTML = `${counter} treats!`;
 app.append(divElement);
 
-//Every second counter goes up
-setInterval(updateCounter, 1000);
+//Every second counter goes up one whole unit
+function updateCounterTime() {
+  counter += (performance.now() - lastTime)/1000;
+  lastTime = performance.now();
+  updateDivElement();
+  requestAnimationFrame(updateCounterTime);
+}
+requestAnimationFrame(updateCounterTime);
+
+//update the Text
+function updateDivElement() {
+  if (counter == 1) {
+    divElement.textContent = `${counter} treat!`; //proper grammar
+  } else {
+    divElement.textContent = `${counter} treats!`;
+  }
+}
+
+
