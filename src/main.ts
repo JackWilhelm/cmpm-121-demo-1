7e3a6cad82370380486731e7f928df2a678b4268
JackWilhelm <jgwilhel@ucsc.edu>
2024-10-08 13:50:23 -0700

    Step 6 complete

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,8 @@ app.append(header);
 //Counter
 let counter: number = 990;
 let lastTime: number = performance.now();
+let rate: number = 0;
+let roundedCounter: string = counter.toFixed(2);
 function updateCounter() {
   counter++;
   console.log(counter);
@@ -44,77 +46,107 @@ const upgradeA: Upgrade = {
   upgradeButtonName: "upgradeAButton",
   cost: 10,
   countersPerSec: 0.1,
-  amountPurchased: 0
-}
+  amountPurchased: 0,
+};
 
 const upgradeB: Upgrade = {
   upgradeButtonName: "upgradeBButton",
   cost: 100,
   countersPerSec: 2,
-  amountPurchased: 0
-}
+  amountPurchased: 0,
+};
 
 const upgradeC: Upgrade = {
   upgradeButtonName: "upgradeCButton",
   cost: 1000,
   countersPerSec: 50,
-  amountPurchased: 0
-}
+  amountPurchased: 0,
+};
 
-const allUpgrades: Upgrade[] = [
-  upgradeA,
-  upgradeB,
-  upgradeC
-];
+const allUpgrades: Upgrade[] = [upgradeA, upgradeB, upgradeC];
 
 function counterRate(): number {
   let result = 0;
   for (const upgrade of allUpgrades) {
-    const currentButton = document.querySelector(`button[name="${upgrade.upgradeButtonName}"]`) as HTMLButtonElement | null;//looking to see if there is a button element with the name
-    if (currentButton) { //check if button is null
+    const currentButton = document.querySelector(
+      `button[name="${upgrade.upgradeButtonName}"]`,
+    ) as HTMLButtonElement | null; //looking to see if there is a button element with the name
+    if (currentButton) {
+      //check if button is null
       if (!currentButton.disabled) {
-        result += ((performance.now() - lastTime) / 1000) * (upgrade.countersPerSec * upgrade.amountPurchased);
+        result +=
+          ((performance.now() - lastTime) / 1000) *
+          (upgrade.countersPerSec * upgrade.amountPurchased);
       }
     }
   }
   return result;
 }
 
+
 //Every second counter goes up one whole unit
 function updateCounterTime() {
-  counter += counterRate() ;
+  counter += counterRate();
   lastTime = performance.now();
   updateDivElement();
+  updateButtonElement();
   unlockUpgrade();
   requestAnimationFrame(updateCounterTime);
 }
 requestAnimationFrame(updateCounterTime);
 
+/*
+let secondCounter = counter;
+setInterval(() => {console.log(rate, (counter - secondCounter).toFixed(1));
+  secondCounter = counter;
+}, 1000);
+*/ //Rate tester
+
 //update the Text
 function updateDivElement() {
-  if (counter == 1) {
-    divElement.textContent = `${counter} treat!`; //proper grammar
+  rate = 0;
+  for (const upgrade of allUpgrades){
+    rate += (upgrade.countersPerSec * upgrade.amountPurchased);
+  }
+  console.log(rate);
+  roundedCounter = counter.toFixed(2);
+  if (roundedCounter == "1") {
+    divElement.innerHTML = `${roundedCounter} treat!<br>${rate} treats/sec`; //proper grammar
   } else {
-    divElement.textContent = `${counter} treats!`;
+    divElement.innerHTML = `${roundedCounter} treats!<br>${rate} treats/sec`;
+  }
+}
+
+//update the button text
+function updateButtonElement() {
+  for (const upgrade of allUpgrades) {
+    const currentButton = document.querySelector(
+      `button[name="${upgrade.upgradeButtonName}"]`,
+    ) as HTMLButtonElement | null; //looking to see if there is a button element with the name
+    if (currentButton) {
+      currentButton.innerHTML = `${currentButton.id}: ${upgrade.amountPurchased}`; 
+    }
   }
 }
 
 //Upgrade buttons
 const upgradeAButton: HTMLButtonElement = document.createElement("button");
-upgradeAButton.innerHTML = "Upgrade A";
+upgradeAButton.innerHTML = `Upgrade A: ${upgradeA.amountPurchased}`;
+upgradeAButton.id = `Upgrade A`;
 upgradeAButton.disabled = true;
 upgradeAButton.name = "upgradeAButton";
 app.append(upgradeAButton);
 
-
 const upgradeBButton: HTMLButtonElement = document.createElement("button");
-upgradeBButton.innerHTML = "Upgrade B";
+upgradeBButton.innerHTML = `Upgrade B: ${upgradeB.amountPurchased}`;
+upgradeBButton.id = `Upgrade B`;
 upgradeBButton.disabled = true;
 upgradeBButton.name = "upgradeBButton";
 app.append(upgradeBButton);
 
 const upgradeCButton: HTMLButtonElement = document.createElement("button");
-upgradeCButton.innerHTML = "Upgrade C";
+upgradeCButton.innerHTML = `Upgrade C: ${upgradeC.amountPurchased}`;
+upgradeCButton.id = `Upgrade C`;
 upgradeCButton.disabled = true;
 upgradeCButton.name = "upgradeCButton";
 app.append(upgradeCButton);
@@ -122,8 +154,11 @@ app.append(upgradeCButton);
 //Disabled until ten
 function unlockUpgrade() {
   for (const upgrade of allUpgrades) {
-    const currentButton = document.querySelector(`button[name="${upgrade.upgradeButtonName}"]`) as HTMLButtonElement | null;//looking to see if there is a button element with the name
-    if (currentButton) { //check if button is null
+    const currentButton = document.querySelector(
+      `button[name="${upgrade.upgradeButtonName}"]`,
+    ) as HTMLButtonElement | null; //looking to see if there is a button element with the name
+    if (currentButton) {
+      //check if button is null
       if (currentButton.disabled && counter >= upgrade.cost) {
         console.log(upgrade.upgradeButtonName);
         currentButton.disabled = false;
